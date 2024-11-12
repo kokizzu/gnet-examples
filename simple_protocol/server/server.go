3a9d21c97b03d338ba386bb674bc4cad6df12fe2
Andy Pan <i@andypan.me>
2024-11-12 22:16:27 +0800

    simple_protocol: support specifying the maximum number of packet to read per event-loop

--- simple_protocol/server/server.go
+++ simple_protocol/server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math"
 	"sync/atomic"
 
 	"github.com/panjf2000/gnet/v2"
@@ -19,6 +20,7 @@ type simpleServer struct {
 	multicore    bool
 	connected    int32
 	disconnected int32
+	batchRead    int // maximum number of packet to read per event-loop iteration
 }
 
 func (s *simpleServer) OnBoot(eng gnet.Engine) (action gnet.Action) {
@@ -51,7 +53,7 @@ func (s *simpleServer) OnClose(c gnet.Conn, err error) (action gnet.Action) {
 func (s *simpleServer) OnTraffic(c gnet.Conn) (action gnet.Action) {
 	codec := c.Context().(*protocol.SimpleCodec)
 	var packets [][]byte
-	for {
+	for i := 0; i < s.batchRead; i++ {
 		data, err := codec.Decode(c)
 		if err == protocol.ErrIncompletePacket {
 			break
@@ -68,21 +70,35 @@ func (s *simpleServer) OnTraffic(c gnet.Conn) (action gnet.Action) {
 	} else if n == 1 {
 		_, _ = c.Write(packets[0])
 	}
+	if len(packets) == s.batchRead && c.InboundBuffered() > 0 {
+		if err := c.Wake(nil); err != nil { // wake up the connection manually to avoid missing the leftover data
+			logging.Errorf("failed to wake up the connection, %v", err)
+			return gnet.Close
+		}
+	}
 	return
 }
 
 func main() {
-	var port int
-	var multicore bool
+	var (
+		port      int
+		multicore bool
+		batchRead int
+	)
 
-	// Example command: go run server.go --port 9000 --multicore=true
+	// Example command: go run server.go --port 9000 --multicore=true --batchread 10
 	flag.IntVar(&port, "port", 9000, "--port 9000")
 	flag.BoolVar(&multicore, "multicore", false, "--multicore=true")
+	flag.IntVar(&batchRead, "batchread", 100, "--batch-read 100")
 	flag.Parse()
+	if batchRead <= 0 {
+		batchRead = math.MaxInt32 // unlimited batch read
+	}
 	ss := &simpleServer{
 		network:   "tcp",
 		addr:      fmt.Sprintf(":%d", port),
 		multicore: multicore,
+		batchRead: batchRead,
 	}
 	err := gnet.Run(ss, ss.network+"://"+ss.addr, gnet.WithMulticore(multicore))
 	logging.Infof("server exits with error: %v", err)
